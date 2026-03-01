2fb2937da3e13a529c46d4834d2d4d78f99c0bcc
İDT <alper3484@gmail.com>
2026-03-02 00:33:49 +0300

    Update sheets.js

--- api/_lib/sheets.js
+++ api/_lib/sheets.js
@@ -30,7 +30,7 @@ async function getSheetData(sheets, sheetName) {
 
 module.exports = async function handler(req, res) {
   // ----- CORS -----
-
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
